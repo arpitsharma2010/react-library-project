0fff87891c5d2259238814b2cd40429b1cdfc812
arpitsharma2010 <arpeet.sharma.1998@gmail.com>
2025-01-08 22:52:46 -0500

    search page by name and category functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { Navbar } from "./Layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./Layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./Layouts/Homepage/HomePage";
+import { SearchBooksPage } from "./Layouts/SearchBooksPage/SearchBooksPage";
 
 export const App = () => {
   return (
     <>
       <Navbar />
-      <HomePage/>
+      {/* <HomePage/> */}
+      <SearchBooksPage />
       <Footer/>
     </>
   );
