1ad59eaa019beead38c81832eb2450358fc01c35
arpitsharma2010 <arpeet.sharma.1998@gmail.com>
2025-01-18 14:38:40 -0500

    added new nav ink and shelf page created

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import { BookCheckoutPage } from "./Layouts/BookCheckoutPage/BookCheckoutPage";
 import { oktaConfig } from "./lib/oktaConfig";
 import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
-import { LoginCallback, Security } from "@okta/okta-react";
+import { LoginCallback, SecureRoute, Security } from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
 import { ReviewListPage } from "./Layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
+import { ShelfPage } from "./Layouts/ShelfPage/ShelfPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -52,6 +53,7 @@ export const App = () => {
           }
         />
         <Route path='/login/callback' component={LoginCallback}/>
+        <SecureRoute path='/shelf'><ShelfPage /></SecureRoute>
       </Switch>
       </div>
       <Footer/>
